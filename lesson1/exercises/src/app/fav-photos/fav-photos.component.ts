271902d584965365f25285ad1bd5aa57417fe794
Ruth Poirier <125494680+PeartreeAndWolf@users.noreply.github.com>
2023-03-09 13:48:08 -0600

    completed chpt 29 exercises part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'I am not 3 opossums stacked on top of each other in a trench coat.';
+  image1 = 'https://t4.ftcdn.net/jpg/03/23/80/77/240_F_323807773_mCcuV7HfQnCqt4IcLIIKONYAaDTQva1X.jpg';
+  image2 = 'https://t4.ftcdn.net/jpg/00/56/96/27/240_F_56962791_o8syVNM8Ojo19TxJhLy1JW6RPHOin2zS.jpg';
+  image3 = 'https://t4.ftcdn.net/jpg/02/71/74/13/240_F_271741306_OrcG9262uRAN5nPA0L5uuY4m7YldvrcA.jpg';
 
   constructor() { }
 
