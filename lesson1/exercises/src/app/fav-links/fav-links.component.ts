42415bff8c76e0180d08507378c84888035d2d7e
Ruth Poirier <125494680+PeartreeAndWolf@users.noreply.github.com>
2023-03-09 18:10:09 -0600

    complete exercises

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fav-links',
+  selector: 'fav-links',
   templateUrl: './fav-links.component.html',
   styleUrls: ['./fav-links.component.css']
 })
 export class FavLinksComponent implements OnInit {
-
+  favLinks = ['https://visitduluth.com/', 'https://en.wikipedia.org/wiki/Lake_Superior', 'https://www.scientificamerican.com/', 'https://www.budgetbytes.com/'];
   constructor() { }
 
   ngOnInit() {
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'I am not 3 opossums stacked on top of each other in a trench coat.';
+  photosTitle = 'Weird Pictures:';
   image1 = 'https://t4.ftcdn.net/jpg/03/23/80/77/240_F_323807773_mCcuV7HfQnCqt4IcLIIKONYAaDTQva1X.jpg';
   image2 = 'https://t4.ftcdn.net/jpg/00/56/96/27/240_F_56962791_o8syVNM8Ojo19TxJhLy1JW6RPHOin2zS.jpg';
   image3 = 'https://t4.ftcdn.net/jpg/02/71/74/13/240_F_271741306_OrcG9262uRAN5nPA0L5uuY4m7YldvrcA.jpg';
